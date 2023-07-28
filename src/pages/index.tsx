e14b42b379e9bd8d17c7cd68921b75789280e298
AmtTawsik <amttawsik.cse@gmail.com>
2023-07-28 16:17:32 +0600

    Add Flowbite

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,7 +7,20 @@ const inter = Inter({ subsets: ['latin'] })
 export default function Home() {
   return (
     <main>
-      
+      <Dropdown label="Dropdown button">
+        <Dropdown.Item>
+          Dashboard
+        </Dropdown.Item>
+        <Dropdown.Item>
+          Settings
+        </Dropdown.Item>
+        <Dropdown.Item>
+          Earnings
+        </Dropdown.Item>
+        <Dropdown.Item>
+          Sign out
+        </Dropdown.Item>
+      </Dropdown>
     </main>
   )
 }
